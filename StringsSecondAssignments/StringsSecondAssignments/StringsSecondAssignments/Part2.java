f38d011c586d337e684ec09970187990a06a951f
erickgnclvs <erickgoncalves5@hotmail.com>
2023-02-07 20:40:03 -0300

    Finish part2 second string assingment

--- StringsSecondAssignments/StringsSecondAssignments/StringsSecondAssignments/Part2.java
+++ StringsSecondAssignments/StringsSecondAssignments/StringsSecondAssignments/Part2.java
@@ -1,33 +1,25 @@
-
-/**
- * Write a description of class Part2 here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Part2
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Part2
-     */
-    public Part2()
-    {
-        // initialise instance variables
-        x = 0;
+public class Part2 {
+    public int howMany(String stringa, String stringb) {
+        int startIndex = 0;
+        int counter = 0;
+        int currIndex = stringb.indexOf(stringa, startIndex);
+        while (true) {
+            if (currIndex != -1) {
+                counter++;
+            } else {
+                break;
+            }
+            currIndex = stringb.indexOf(stringa, currIndex + stringa.length());
+        }
+        return counter;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void testHowMany() {
+        String stringa = "GAA";
+        String stringb = "ATGAACGAATTGAATC";
+        System.out.println(howMany(stringa, stringb));
+        stringa = "AA";
+        stringb = "ATAAAATAAAA";
+        System.out.println(howMany(stringa, stringb));
     }
 }
